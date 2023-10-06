05c92b5316e9f10c3c97c129a448e6366a1e3386
agotab <agota0201@gmail.com>
2023-10-06 19:07:42 +0200

    dom manipulation callback

--- script.js
+++ script.js
@@ -1,81 +1,23 @@
-let x = "asd"
-let y = 12
-let z = false
+console.log("hello world")
 
-let arr = ["egy", "ketto", "harom"]
-let arr2 = [10, 2, 67]
-let arr3 = [true, false, true]
-let arr4 = ["string", 1, false]
-let obj1 = {
-    key1: "value",
-    key2: 101,
-
-key3: false,
-key4: [
-    "cat",
-    "dog",
-    "chicken"
-],
-key5: {
-    nestedKey: "text",
-    nestedKey2: 66
-},
-users: [
-    {
-        name: "Gipsz Jakab",
-        age: 28,
-        isActive: true,
-        pets: [
-            {
-            name: "Mirci",
-            type: "cat"
-        },
-        {
-            name: "Kifli",
-            type: "dog"
-        }
-    ]
-    },
-    {
-        name: "John Doe",
-        age: 66,
-        isActive: false,
-        pets: ["Jani", "Laci"]
+function domManipulation() {
+    const rootElement = document.querySelector('#root')
+    console.log(rootElement)
+    rootElement.innerHTML = "this is done by dom manipulation"
     }
-]
-}
-// console.log(obj1.users[0].pets[0].name) //dot notation
-// console.log(obj1["users"][0]["pets"][0]["name"]) //bracket notation
 
-function myFunc() {
-    console.log("this is my function")
-}
+    function logClick() {
+        console.log('clicked')
+    }
 
- // myFunc()
+    let tickCount = 0
 
-function greetMe(name) {
-    console.log(`hello ${name}!`)
-}
+    function logTick() {
+        console.log(`tick ${tickCount}`)
+        tickCount++
+    }
 
-greetMe("Agota")
+window.addEventListener('load', domManipulation)
+window.addEventListener('click', logClick)
 
-/* function addTwoNumbers(number1, number2) {
-    let sum = number1 + number2
-    console.log(`the sum of two numbers (${number1}, ${number2}) is: ${sum}`)
-    return sum
-}
-
-let resultOfAddTwoNumbers = addTwoNumbers(10, 2)
-
-console.log(`the result of resultOfAddTwoNumbers is: ${resultOfAddTwoNumbers}`) */
-
-function cbExample() {
-    console.log("i am a callback function")
-}
-
-function funcExample(name, callback) {
-    console.log(`hello ${name}`)
-    callback()
-}
-
-funcExample("Ricsi", cbExample)
+window.setInterval(logTick, 1000)
